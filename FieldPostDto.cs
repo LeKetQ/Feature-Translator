e2c1dbaa6e1d977155c8cee8c0b77606dc77c254
LeKetQ <quentindemunck@hotmail.com>
2023-11-10 14:30:09 +0100

    Break Stuff

---
+++ FieldPostDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Feature_Translator;
+public class FieldPostDto
+{
+    [DataMember(Name = "id")]
+    public int Id { get; set; }
+
+    [DataMember(Name = "templateFieldId")]
+    public int TemplateFieldId { get; set; }
+
+    [DataMember(Name = "value")]
+    public string Value { get; set; }
+}
--- TranslatorScript.cs
+++ TranslatorScript.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 class TranslatorScript
 {
-    int successes = 0;
-    int failures = 0;
-    StringBuilder stringBuilder = new StringBuilder();
+    private int successes = 0;
+    private int failures = 0;
+    private StringBuilder stringBuilder = new StringBuilder();
+    const string apiUrl = "https://tide-travelworld8527-staging.azurewebsites.net/api/";
+    const string NL = "nl-BE";
+    const string FR = "fr-BE";
 
     static async Task Main()
     {
@@ -24,155 +27,13 @@ class TranslatorScript
 
             if (!response.IsSuccessStatusCode)
             {
-                await LogFailure("Login", response);
+                ShowConsoleFeedback("Login", response, null);
                 return;
             }
 
             // Process each level of children recursively
             await ProcessLevel(client, null);
         }
-    }
-
-    private async Task ProcessLevel(HttpClient client, int? parentId)
-    {
-        var response = await GetChildren(client, parentId);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            await LogFailure($"GetChildren for {parentId} failed", response);
-            return;
-        }
-
-        var content = await ReadContent(response);
-        var children = JsonConvert.DeserializeObject<List<ContentId>>(content);
-
-        foreach (var child in children)
-        {
-            if (int.TryParse(child.Id, out var childId))
-            {
-                // GET, PROCESS AND PUT DATA 
-                await ProcessContent(client, childId);
-
-                // Recursively process next level
-                await ProcessLevel(client, childId);
-            }
-        }
-    }
-
-    private async Task LogFailure(string action, HttpResponseMessage response)
-    {
-        Console.WriteLine($"{action} failed \n");
-        Console.WriteLine($"{await ReadContent(response)} \n");
-    }
-
-    private async Task<HttpResponseMessage> ProcessContent(HttpClient client, int id)
-    {
-        var response = await GetContentData(client, id);
-
-        if (response.IsSuccessStatusCode)
-        {
-            var content = await ReadContent(response);
-            var fields = JsonConvert.DeserializeObject<List<Field>>(content);
-
-            // GET OVERHEAD OBJECT
-            response = await GetOverheadObject(client, id);
-
-            if (response.IsSuccessStatusCode)
-            {
-                content = await ReadContent(response);
-                var finalContent = JsonConvert.DeserializeObject<Content>(content);
-
-                // MAP THE TRANSLATED VALUE
-                foreach (var field in fields.Where(f => f.DataType == 0))
-                {
-                    foreach (var finalContentField in finalContent.Fields)
-                    {
-                        if (finalContentField.TemplateFieldId == field.TemplateFieldId && string.IsNullOrEmpty(finalContentField.Value))
-                        {
-                            finalContentField.Value = await DeepleTranslate(client, field.Value);
-                        }
-                    }
-                }
-
-                if (finalContent.ContentItemChannelId == -1)
-                {
-                    finalContent.ContentItemChannelId = await GetContentItemId(client, id);
-                }
-                finalContent.IsContentItemChannelActive = true;
-                finalContent.Language = "fr-BE";
-
-                response = await PutContent(client, finalContent);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    this.successes++;
-                }
-                else
-                {
-                    this.failures++;
-
-                    var failedId = $"{finalContent.Id}";
-                    this.stringBuilder.AppendLine($"{failedId.PadRight(5)} - Failed");
-                }
-                Console.Clear();
-                Console.WriteLine($"Successes: {this.successes}");
-                Console.WriteLine($"Failures:  {this.failures}");
-                Console.WriteLine($"{this.stringBuilder}");
-            }
-        }
-
-        return response;
-    }
-
-    private async Task<HttpResponseMessage> GetOverheadObject(HttpClient client, int id)
-    {
-        return await client.GetAsync($"https://tide-travelworld8527-staging.azurewebsites.net/api/content-item/{id}/Internal/fr-BE/1");
-    }
-
-    private async Task<int?> GetContentItemId(HttpClient client, int id)
-    {
-        var response = await client.GetAsync($"https://tide-travelworld8527-staging.azurewebsites.net/api/content-item/{id}/Internal/nl-BE/1");
-        var dutchOverheadObject = await ReadContent(response);
-        var finalObject = JsonConvert.DeserializeObject<Content>(dutchOverheadObject);
-
-        if (finalObject.ContentItemChannelId == -1)
-        {
-            response = await client.GetAsync($"https://tide-travelworld8527-staging.azurewebsites.net/api/content-item/{id}/Web/nl-BE/1");
-            dutchOverheadObject = await ReadContent(response);
-            finalObject = JsonConvert.DeserializeObject<Content>(dutchOverheadObject);
-        }
-
-        return finalObject.ContentItemChannelId;
-    }
-
-    private async Task<HttpResponseMessage> PutContent(HttpClient client, Content content)
-    {
-        string apiUrl = $"https://tide-travelworld8527-staging.azurewebsites.net/api/content-item";
-
-        // PUT TO WEB
-        content.ChannelId = 3; // CHANNEL 3 = WEB
-        var rawPayload = JsonConvert.SerializeObject(content);
-        var payload = new StringContent(rawPayload, Encoding.UTF8, "application/json");
-        var response = await client.PutAsync(apiUrl, payload);
-
-        return response;
-    }
-
-    private async Task<string> DeepleTranslate(HttpClient client, string data)
-    {
-        var deepleApiUrl = "https://tide-travelworld8527-staging.azurewebsites.net/api/deepl";
-        var deepleRawPayload = $"{{text: \"{data}\", from: \"NL\", to: \"FR\"}}";
-        var deeplePayload = new StringContent(deepleRawPayload, Encoding.UTF8, "application/json");
-        var deepleResponse = await client.PostAsync(deepleApiUrl, deeplePayload);
-
-        if (deepleResponse.IsSuccessStatusCode)
-        {
-            data = await deepleResponse.Content.ReadAsStringAsync();
-        }
-
-        data.TrimStart('"').TrimEnd('"');
-
-        return data;
     }
 
     private async Task<HttpResponseMessage> Login(HttpClient client)
@@ -189,24 +50,108 @@ class TranslatorScript
         return await client.PostAsync(signinApiUrl, content);
     }
 
+    private async Task ProcessLevel(HttpClient client, int? parentId)
+    {
+        var response = await GetChildren(client, parentId);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            ShowConsoleFeedback("GetChildren", response, parentId);
+            return;
+        }
+
+        var content = await ReadContent(response);
+        var children = JsonConvert.DeserializeObject<List<ContentId>>(content);
+
+        foreach (var child in children)
+        {
+            if (int.TryParse(child.Id, out var childId))
+            {
+                // Process current child
+                await ProcessContent(client, childId);
+
+                // Recursively process next level
+                await ProcessLevel(client, childId);
+            }
+        }
+    }
+
+    private async Task<HttpResponseMessage> ProcessContent(HttpClient client, int id)
+    {
+        var response = await GetFrenchContent(client, id);
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await ReadContent(response);
+            var frenchContent = JsonConvert.DeserializeObject<Content>(content);
+
+            if (frenchContent.Version == 0)
+            {
+                response = await GetFieldsContent(client, id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    content = await ReadContent(response);
+                    var fields = JsonConvert.DeserializeObject<List<Field>>(content);
+                    if (fields != null && fields.Any())
+                    {
+                        frenchContent.Fields = new List<Field>();
+
+                        foreach (var field in fields)
+                        {
+                            var frenchField = new Field()
+                            {
+                                Id = -1,
+                                TemplateFieldId = field.TemplateFieldId,
+                            };
+
+                            if (field.DataType == 0)
+                            {
+                                frenchField.Value = await DeepleTranslate(client, field.Value);
+                            }
+                            else
+                            {
+                                frenchField.Value = field.Value;
+                            }
+
+                            frenchContent.Fields.Add(frenchField);
+                        }
+                    }
+                }
+
+                frenchContent.IsContentItemChannelActive = true;
+                frenchContent.ChannelId = 3;
+                frenchContent.Version = 1;
+                response = await PutContent(client, frenchContent);
+                ShowConsoleFeedback("PUT", response, id);
+            }
+        }
+
+        return response;
+    }
+
     private async Task<HttpResponseMessage> GetChildren(HttpClient client, int? parentId)
     {
         if (parentId == null)
         {
             // ROOT
-            return await client.GetAsync("https://tide-travelworld8527-staging.azurewebsites.net/api/content-item-tree/children");
+            return await client.GetAsync($"{apiUrl}content-item-tree/children");
         }
         else
         {
             // CHILDREN FROM PARENT ID
-            return await client.GetAsync($"https://tide-travelworld8527-staging.azurewebsites.net/api/content-item-tree/children/{parentId}");
+            return await client.GetAsync($"{apiUrl}content-item-tree/children/{parentId}");
         }
     }
 
-    private async Task<HttpResponseMessage> GetContentData(HttpClient client, int id)
+    private async Task<HttpResponseMessage> GetFrenchContent(HttpClient client, int id)
     {
-        string postApiUrl = "https://tide-travelworld8527-staging.azurewebsites.net/api/content-item/inheritance";
-        var rawPayload = $"{{contentItemId: {id}, channel: \"Web\", languageCode: \"nl-BE\"}}";
+        return await client.GetAsync($"{apiUrl}content-item/{id}/Web/fr-BE/1");
+    }
+
+    private async Task<HttpResponseMessage> GetFieldsContent(HttpClient client, int id)
+    {
+        string postApiUrl = $"{apiUrl}content-item/inheritance";
+        var rawPayload = $"{{contentItemId: {id}, channel: \"Internal\", languageCode: \"nl-BE\"}}";
         var payload = new StringContent(rawPayload, Encoding.UTF8, "application/json");
         return await client.PostAsync(postApiUrl, payload);
     }
@@ -214,5 +159,65 @@ class TranslatorScript
     private async Task<string> ReadContent(HttpResponseMessage response)
     {
         return await response.Content.ReadAsStringAsync();
+    }
+
+    [Obsolete]
+    private async Task<HttpResponseMessage> GetDutchContent(HttpClient client, int id)
+    {
+        return await client.GetAsync($"{apiUrl}content-item/{id}/Web/nl-BE/1");
+    }
+
+    private async Task<string> DeepleTranslate(HttpClient client, string data)
+    {
+        var deepleApiUrl = $"{apiUrl}deepl";
+        var deepleRawPayload = $"{{text: \"{data}\", from: \"NL\", to: \"FR\"}}";
+        var deeplePayload = new StringContent(deepleRawPayload, Encoding.UTF8, "application/json");
+        var deepleResponse = await client.PostAsync(deepleApiUrl, deeplePayload);
+
+        if (deepleResponse.IsSuccessStatusCode)
+        {
+            data = await deepleResponse.Content.ReadAsStringAsync();
+        }
+
+        data.TrimStart('"').TrimEnd('"');
+
+        return data;
+    }
+
+    [Obsolete]
+    private async Task<int?> GetContentItemChannelId(HttpClient client, int id)
+    {
+        var response = await client.GetAsync($"{apiUrl}content-item/{id}/Web/fr-BE/1");
+        var overheadObject = await ReadContent(response);
+        var finalObject = JsonConvert.DeserializeObject<Content>(overheadObject);
+        return finalObject.ContentItemChannelId;
+    }
+
+    private async Task<HttpResponseMessage> PutContent(HttpClient client, Content content)
+    {
+        string url = $"{apiUrl}content-item";
+        var rawPayload = JsonConvert.SerializeObject(content);
+        var payload = new StringContent(rawPayload, Encoding.UTF8, "application/json");
+        var response = await client.PutAsync(url, payload);
+
+        return response;
+    }
+
+    private void ShowConsoleFeedback(string action, HttpResponseMessage response, int? id)
+    {
+        if (response.IsSuccessStatusCode)
+        {
+            this.successes++;
+            this.stringBuilder.AppendLine($"{id} - {action} SUCCESS \n");
+        }
+        else
+        {
+            this.failures++;
+        }
+
+        Console.Clear();
+        Console.WriteLine($"Successes: {this.successes}");
+        Console.WriteLine($"Failures:  {this.failures}");
+        Console.WriteLine($"{this.stringBuilder}");
     }
 }
